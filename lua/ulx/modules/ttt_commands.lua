fda352126c131b6563ee24262dce4ec63fcfeaef
zachcheatham <zachcheatham@gmail.com>
2015-01-29 20:31:58 -0600

    Fixed up addslay. Added voting commands.

--- lua/ulx/modules/ttt_commands.lua
+++ lua/ulx/modules/ttt_commands.lua
@@ -1,31 +1,11 @@
-local PDATA_KEY = "ttt_pending_slay"
+-----------------
+-- TTT AddSlay --
+-----------------
 
---[[local function findRagdoll(ply)
-	for _, ent in pairs( ents.FindByClass( "prop_ragdoll" )) do
-		if ent.uqid == ply:UniqueID() and IsValid(ent) then
-			return ent or false
-		end
-	end
-end
+local SLAY_PDATA_KEY = "ttt_pending_slay"
 
-local function identifyBody(ply)
-	if not CORPSE.GetFound(rag, false) then
-		ply:SetNWBool("body_found", true)
-		
-		if ply:GetTraitor() then
-			SendConfirmedTraitors(GetInnocentFilter(false))
-		end
-		
-		local ragdoll = findRagdoll(ply)
-		if ragdoll then
-			--hook.Call("TTTBodyFound", GAMEMODE, ply, ply, ragdoll)
-			CORPSE.SetFound(ragdoll, true)
-		end
-	end
-end]]--
-
-local function getSlayInfo(ply)
-	local slayString = ply:GetPData(PDATA_KEY, false)
+function ulx.getSlayInfo(ply)
+	local slayString = ply:GetPData(SLAY_PDATA_KEY, false)
 
 	if not slayString then
 		return 0, false
@@ -43,34 +23,32 @@ local function getSlayInfo(ply)
 				
 				return slays, reason
 			else
-				ply:RemovePData(PDATA_KEY)
+				ply:RemovePData(SLAY_PDATA_KEY)
 				return 0, false
 			end
 		else
-			ply:RemovePData(PDATA_KEY)
+			ply:RemovePData(SLAY_PDATA_KEY)
 			return 0, false
 		end
 	end
 end
 
-local function roundBegin()
+local function slayListRoundBegin()
 	for _, ply in pairs(player.GetAll()) do
 		if ply:Alive() then
-			local slays, reason = getSlayInfo(ply)
+			local slays, reason = ulx.getSlayInfo(ply)
 			
 			if slays > 0 then
 				-- Kill
 				ply:Kill()
-				-- Identifing the body seems to fuck with the scoreboard
-				--timer.Simple(1, function() identifyBody(ply) end)
-			
+				
 				-- Save data for next round
 				slays = slays - 1
 				if slays > 0 then
 					local slayString = slays .. ";" .. reason
-					ply:SetPData(PDATA_KEY, slayString)
+					ply:SetPData(SLAY_PDATA_KEY, slayString)
 				else
-					ply:RemovePData(PDATA_KEY)
+					ply:RemovePData(SLAY_PDATA_KEY)
 				end
 				
 				-- Tell everyone what happened and maybe they won't kill each other
@@ -79,4 +57,57 @@ local function roundBegin()
 		end
 	end
 end
-hook.Add("TTTBeginRound", "ULXSlayList", roundBegin)
+hook.Add("TTTBeginRound", "ULXSlayList", slayListRoundBegin)
+
+-----------------------
+-- Vote Silent Round --
+-----------------------
+local function setULXGag(ply, gagged)
+	ply.ulx_gagged = gagged
+	ply:SetNWBool("ulx_gagged", gagged)
+end
+
+ulx.nextRoundSilent = false
+ulx.inSilentRound = false
+
+local gaggedPlayers = {}
+
+function ulx.initiateSilentRound()
+	if not ulx.inSilentRound then
+		ulx.inSilentRound = true
+		table.Empty(gaggedPlayers)
+		
+		for _, ply in ipairs(player.GetAll()) do
+			if not ply.ulx_gagged then
+				table.insert(gaggedPlayers, ply)
+				setULXGag(ply, true)
+			end
+		end
+		
+		ULib.tsayColor(nil, false, Color(201, 155, 255), "Silent round initialized. ", Color(255, 255, 255), "You will not be able to voice chat until the end of the round.")
+	end
+end
+
+local function vsrRoundBegin()
+	if ulx.nextRoundSilent then
+		ulx.nextRoundSilent = false
+		ulx.initiateSilentRound()
+	end
+end
+hook.Add("TTTBeginRound", "ULXVoteSilentRound", vsrRoundBegin)
+
+local function vsrRoundEnd()
+	if ulx.inSilentRound then
+		ulx.inSilentRound = false
+		
+		for _, ply in ipairs(gaggedPlayers) do
+			if IsValid(ply) then
+				setULXGag(ply, false)
+			end
+		end
+		
+		table.Empty(gaggedPlayers)
+		ULib.tsayColor(nil, false, Color(100, 255, 0), "Silent round ended. ", Color(255, 255, 255), "You may now use voice chat.")
+	end
+end
+hook.Add("TTTEndRound", "ULXVoteSilentRound", vsrRoundEnd)
