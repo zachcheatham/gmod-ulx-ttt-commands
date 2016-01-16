f772256d3e1ba968ad058adfc25677cee466d302
Zach Cheatham <zachcheatham@gmail.com>
2016-01-16 09:12:05 -0600

    Replace automatic module detection.

--- lua/ulx/modules/sh/ttt_cmds.lua
+++ lua/ulx/modules/sh/ttt_cmds.lua
@@ -9,7 +9,18 @@ end
 hook.Add(ULib.HOOK_UCLCHANGED, "ULXRoleNamesUpdate", updateRoles)
 updateRoles()
 
-local modules, dirs = file.Find("ulx/modules/sh/ttt_cmds/*lua", "LUA")
+-- This isn't working on Linux
+--local modules, dirs = file.Find("ulx/modules/sh/ttt_cmds/*lua", "LUA")
+
+local modules = {
+    -- "ttt_cmds_autopunish.lua", -- We have a new addon for this :)
+    "ttt_cmds_corpses.lua",
+    "ttt_cmds_force_role.lua",
+    "ttt_cmds_karma.lua",
+    "ttt_cmds_respawn.lua",
+    "ttt_cmds_spectate.lua",
+    "ttt_cmds_voting.lua"
+}
 
 for _, fileName in ipairs(modules) do
 	if SERVER then
